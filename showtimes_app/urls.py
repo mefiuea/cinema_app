e91587b69818363939d7290266ef91ee0aae1b17
Maciej Koczot <mefiuea@gmail.com>
2022-05-10 15:04:05 +0200

    upgrade views: add screeningListView and ScreeningView; upgrade urls

--- showtimes_app/urls.py
+++ showtimes_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from showtimes_app.views import CinemaListView, CinemaView
+from showtimes_app.views import CinemaListView, CinemaView, ScreeningListView, ScreeningView
 
 app_name = 'showtimes_app'
 
 urlpatterns = [
     path('cinemas/', CinemaListView.as_view(), name='cinema_list_view'),
     path('cinemas/<int:pk>/', CinemaView.as_view(), name='cinema_view'),
+    path('screenings/', ScreeningListView.as_view()),
+    path('screenings/<int:pk>/', ScreeningView.as_view()),
 ]
--- showtimes_app/views.py
+++ showtimes_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 
-from showtimes_app.models import Cinema
-from showtimes_app.serializers import CinemaSerializer
+from showtimes_app.models import Cinema, Screening
+from showtimes_app.serializers import CinemaSerializer, ScreeningSerializer
 
 
 class CinemaListView(generics.ListCreateAPIView):
@@ -13,3 +13,13 @@ class CinemaListView(generics.ListCreateAPIView):
 class CinemaView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+
+
+class ScreeningListView(generics.ListCreateAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
+
+
+class ScreeningView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Screening.objects.all()
+    serializer_class = ScreeningSerializer
