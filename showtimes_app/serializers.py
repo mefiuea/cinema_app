a8185fdd2a628900f100f88e0a5c9f241bcf6191
Maciej Koczot <mefiuea@gmail.com>
2022-05-10 14:43:36 +0200

    add tests

--- showtimes_app/serializers.py
+++ showtimes_app/serializers.py
@@ -4,7 +4,7 @@ from showtimes_app.models import Cinema
 
 
 class CinemaSerializer(serializers.ModelSerializer):
-    movies = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='movies-detail')
+    movies = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='movielist_app:movie_view')
 
     class Meta:
         model = Cinema
